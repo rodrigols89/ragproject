bc0a3fa70bbf5bb73f16260f540299a38203003b
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-12-25 23:11:47 -0300

    refactor(routes): Improve and refactor url.py implementation

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("users.urls")),
-    path(  # Rotas do django-allauth
+    path(
+        "admin/",
+        admin.site.urls
+    ),
+    path(
         "accounts/",
         include("allauth.urls")
+    ),
+    path(
+        "",
+        include("users.urls")
+    ),
+    path(
+        "",
+        include("workspace.urls")
     ),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
 
-from .views import create_account, home_view, login_view, logout_view
+from . import views
 
 urlpatterns = [
-    path(route="", view=login_view, name="index"),
-    path(route="home/", view=home_view, name="home"),
-    path(route="logout/", view=logout_view, name="logout"),
+    path(
+        route="",
+        view=views.login_view,
+        name="index"
+    ),
+    path(
+        route="home/",
+        view=views.home_view,
+        name="home"
+    ),
+    path(
+        route="logout/",
+        view=views.logout_view,
+        name="logout"
+    ),
     path(
         route="create-account/",
-        view=create_account,
+        view=views.create_account,
         name="create-account"
     ),
 ]
